4a5bbf2ebc69f432c78be33a3683919e56312681
Saddam Hossain <saddamh1220@gmail.com>
2023-07-22 21:18:41 +0600

    subsink package added

--- src/app/modules/doctors/doctor-list/doctor-list.component.ts
+++ src/app/modules/doctors/doctor-list/doctor-list.component.ts
@@ -3,6 +3,7 @@ import { IDoctorListResponse } from '../doctor.models';
 import { DoctorService } from '../doctor.service';
 import { environment } from 'src/environments/environment';
 import { Observable, Subscription } from 'rxjs';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-doctor-list',
@@ -14,6 +15,8 @@ export class DoctorListComponent implements OnInit, OnDestroy {
 
   apiUrl = environment.apiUrl;
   doctors$: Observable<IDoctorListResponse>;
+
+  subscriptions: SubSink;
 
   // doctorSubscription: Subscription;
 
@@ -30,13 +33,20 @@ export class DoctorListComponent implements OnInit, OnDestroy {
     //   }
     // });
 
-    // Receiving data from api as Stream.
+    // this.subscriptions.sink = this.doctorService.getDoctors(`${this.apiUrl}/doctors`).subscribe(response => {
+    //   if(response.success){
+    //     this.doctors = response.doctors;
+    //   }
+    // });
+
+    // Receiving data from api as Stream. 
     this.doctors$ = this.doctorService.getDoctors(`${this.apiUrl}/doctors`);
   }
 
   ngOnDestroy(): void {
     console.log("Doctor list component destroyed");
     // this.doctorSubscription.unsubscribe();
+    // this.subscriptions.unsubscribe();
   }
 
 
